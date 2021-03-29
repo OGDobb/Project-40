f6996ffb50d16c23d8cd84bf12e7edbd54cac210
OGDobb <73088984+OGDobb@users.noreply.github.com>
2021-03-28 20:33:36 -0700

    Add files via upload

--- js/game.js
+++ js/game.js
@@ -32,7 +32,6 @@ class Game{
     player2 = createSprite(800,500);
     player2.addImage("player2", player_img);
     players=[player1,player2];
-
         }
     
     play(){
@@ -41,25 +40,25 @@ class Game{
 
                 Player.getPlayerInfo();
                  image(back_img, 0, 0, 1000, 800);
-                 //var x=100;
-                 //var y=200;
+                 var x=100;
+                 var y=200;
                  var index =0;
                  drawSprites();
                  for(var plr in allPlayers){
                     
                     
                      index = index+1;
-                    //x = 500-allPlayers[plr].distance;
-                    //y=500;
+                    x = 1000-allPlayers[plr].distance;
+                    y=500;
                      
-                     //players[index -1].x = x;
-                     //players[index - 1].y = y;
+                     players[index -1].x = x;
+                     players[index - 1].y = y;
                        
                      if(index === player.index){
                          
                          fill("black");
                          textSize(25);
-                        // text(allPlayers[plr].name ,x-25,y+25);
+                         text(allPlayers[plr].name ,x-25,y+20);
 
                          
                      }
--- sketch.js
+++ sketch.js
@@ -12,7 +12,7 @@ var fruitGroup;
 var fruit1_img, fruit2_img, fruit3_img, fruit4_img, fruit5_img;
 var player_img;
 
-var carsRank;
+var playerRank;
 
 var endMsgWrite = true;
 
